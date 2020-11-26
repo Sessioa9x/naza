86a1d3f41f1bfa87ab930410a0e272c341c4d08a
Sessioa9x <dylan.lagarde25@gmail.com>
2020-11-26 10:28:43 +0100

    navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Screens/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
---
+++ Screens/Home.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import axios from 'axios';
+
+const Home  = () => {
+    
+    return(
+        <View style={styles.container}>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection:'row',
+        justifyContent:'center',
+        backgroundColor: '#fff',
+      },
+});
+
+export default Home
