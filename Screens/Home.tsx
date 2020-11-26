72e0a7a8c739dce97c682a54050d463d49dfe0ff
Sessioa9x <dylan.lagarde25@gmail.com>
2020-11-26 11:11:08 +0100

    appel de l'api

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import axios from 'axios';
+import { Galaxie } from '../Type';
+
+
 
 const Home  = () => {
-    
+    const [galaxie, setGalaxie] = useState<Galaxie>()
+
+    useEffect(()=>{
+        const asyncFunction = async () => {
+            const res = await axios.get('https://api.nasa.gov/planetary/apod?api_key=Efs2dOcIFao5dYnZNxNfvBfjMqSCwuLrKh7ri3Py')
+            
+            setGalaxie(res.data);
+        }
+        asyncFunction();
+    }, [])
+
+    console.log(galaxie);
     return(
         <View style={styles.container}>
         </View>
